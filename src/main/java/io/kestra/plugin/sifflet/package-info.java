54e0b3ab3ad8e2e5e0b19a8d487df4e650f0099e
François Delbrayelle <fdelbrayelle@kestra.io>
2026-02-12 11:31:05 +0100

    chore: merge package categories attribute

--- src/main/java/io/kestra/plugin/sifflet/package-info.java
+++ src/main/java/io/kestra/plugin/sifflet/package-info.java
@@ -1,5 +1,4 @@
-@PluginSubGroup(title = "Sifflet", description = "This plugin provides tasks for interacting with Sifflet data quality platform.", categories = PluginSubGroup.PluginCategory.TOOL,
-    categories = { PluginSubGroup.PluginCategory.DATA }
+@PluginSubGroup(title = "Sifflet", description = "This plugin provides tasks for interacting with Sifflet data quality platform.",categories = { PluginSubGroup.PluginCategory.DATA }
 )
 package io.kestra.plugin.sifflet;
 
