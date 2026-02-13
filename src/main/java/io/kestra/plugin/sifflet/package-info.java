ed44a0d11804a5cdcd3546192e942ecf1b016192
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 09:06:17 +0100

    chore: update plugin categories (#25)

--- src/main/java/io/kestra/plugin/sifflet/package-info.java
+++ src/main/java/io/kestra/plugin/sifflet/package-info.java
@@ -1,4 +1,5 @@
-@PluginSubGroup(title = "Sifflet", description = "This plugin provides tasks for interacting with Sifflet data quality platform.", categories = PluginSubGroup.PluginCategory.TOOL)
+@PluginSubGroup(title = "Sifflet", description = "This plugin provides tasks for interacting with Sifflet data quality platform.",categories = { PluginSubGroup.PluginCategory.DATA }
+)
 package io.kestra.plugin.sifflet;
 
 import io.kestra.core.models.annotations.PluginSubGroup;
